83bf4f7a867fe9f76f9ac936119c806c96de39e6
Felipefreitas020 <felipefreitasreboucas@gmail.com>
2024-09-18 21:13:10 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+let continuar = true;
+
+while (continuar) {
+    
+    let cavalariaCarro1 = parseInt(prompt("Digite a cavalaria do Carro 1:"));
+    let cavalariaCarro2 = parseInt(prompt("Digite a cavalaria do Carro 2:"));
+    
+    if (cavalariaCarro1 > cavalariaCarro2) {
+        alert("Carro 1 é o vencedor!");
+    } else if (cavalariaCarro2 > cavalariaCarro1) {
+        alert("Carro 2 é o vencedor!");
+    } else {
+        alert("É um empate!");
+    }
+
+    
+    let opcao;
+    do {
+        opcao = prompt("Deseja fazer outra corrida? (s/n)").toLowerCase();
+    } while (opcao !== 's' && opcao !== 'n');
+
+    
+    switch (opcao) {
+        case 's':
+            continuar = true;
+            break;
+        case 'n':
+            continuar = false;
+            alert("Obrigado por participar da corrida!");
+            break;
+        default:
+            continuar = false;
+            alert("Opção inválida! Encerrando.");
+    }
+}
