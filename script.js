e70839369d6fc8969e15ee1e8390016891918ff3
Felipefreitas020 <felipefreitasreboucas@gmail.com>
2024-09-19 07:32:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-let continuar = true;
+let continuar = true
 
 while (continuar) {
     
-    let cavalariaCarro1 = parseInt(prompt("Digite a cavalaria do Carro 1:"));
-    let cavalariaCarro2 = parseInt(prompt("Digite a cavalaria do Carro 2:"));
+    let cavalariaCarro1 = parseInt(prompt("Digite a cavalaria do Carro 1:"))
+    let cavalariaCarro2 = parseInt(prompt("Digite a cavalaria do Carro 2:"))
     
     if (cavalariaCarro1 > cavalariaCarro2) {
-        alert("Carro 1 é o vencedor!");
+        alert("Carro 1 é o vencedor!")
     } else if (cavalariaCarro2 > cavalariaCarro1) {
-        alert("Carro 2 é o vencedor!");
+        alert("Carro 2 é o vencedor!")
     } else {
-        alert("É um empate!");
+        alert("É um empate!")
     }
 
     
-    let opcao;
+    let opção
     do {
-        opcao = prompt("Deseja fazer outra corrida? (s/n)").toLowerCase();
-    } while (opcao !== 's' && opcao !== 'n');
+        opcao = prompt("Deseja fazer outra corrida? (s/n)").toLowerCase()
+    } while (opcao !== 's' && opcao !== 'n')
 
     
     switch (opcao) {
         case 's':
-            continuar = true;
-            break;
+            continuar = true
+            break
         case 'n':
-            continuar = false;
-            alert("Obrigado por participar da corrida!");
-            break;
-        default:
-            continuar = false;
-            alert("Opção inválida! Encerrando.");
+            continuar = false
+            alert("Obrigado por participar da corrida!")
+            break
+        default
+            continuar = false
+            alert("Opção inválida! Encerrando.")
     }
 }
